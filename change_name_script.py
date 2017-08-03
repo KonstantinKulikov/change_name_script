d0958bad5d687da5c757607002598a9cc486bf73
KonstantinKulikov <sqarelem@gmail.com>
2017-08-03 20:12:31 +0300

    Update change_name_script.py

--- change_name_script.py
+++ change_name_script.py
@@ -1,10 +1,18 @@
 import os
 
-
-dir = '/Users/UserName/Documents/Image/'                                            #указываем директорию в которой лежат файлы
+#указываем директорию в которой лежат файлы
+dir = '/Users/UserName/Documents/Image/'                                            
 
 for file in os.listdir(dir):
-       print(file+' -> '+ '{0}-{1}.jpg'.format(file[6:11], file[0:5]))              #выводим список изменяемых файлов в консоль, указываем какие части названия файла перемещаем местами
-       previous_name = os.path.join(dir, file)                                      #соединяем путь к исходному файлу
-       new_name = os.path.join(dir, '{0}-{1}.jpg'.format(file[6:11], file[0:5]))    #соединяем путь к переименованному файлу
-       os.rename(previous_name, new_name)                                           #переименовываем файл
+       #выводим список изменяемых файлов в консоль, 
+       #указываем какие части названия файла перемещаем местами
+       print(file+' -> '+ '{0}-{1}.jpg'.format(file[6:11], file[0:5]))
+       
+       #соединяем путь к исходному файлу
+       previous_name = os.path.join(dir, file)
+       
+       #соединяем путь к переименованному файлу
+       new_name = os.path.join(dir, '{0}-{1}.jpg'.format(file[6:11], file[0:5]))
+       
+       #переименовываем файл
+       os.rename(previous_name, new_name)
